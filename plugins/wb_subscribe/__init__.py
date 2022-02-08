08b8ad26d178fb85365e0ca765072d8eceb9fa71
Dr.Ink <x2590633525@outlook.com>
2022-02-08 15:24:50 +0800

    feat 微博退订&修改部分处理逻辑

--- plugins/wb_subscribe/__init__.py
+++ plugins/wb_subscribe/__init__.py
@@ -17,17 +17,23 @@ ls_wb_subscribe = on_command('微博订阅列表', priority=5)
 watch = require("nonebot_plugin_apscheduler").scheduler
 
 
-@watch.scheduled_job("interval", minutes=5, max_instances=5)
-# @watch.scheduled_job("interval", seconds=30, max_instances=5)
-async def run_every_10_min():
-    logger.info('开始检查微博更新...')
+@watch.scheduled_job("cron", hour='22-23,0-7', minute='*/10', jitter=10)
+@watch.scheduled_job("cron", hour='8-21', minute='*/2', jitter=10)
+# @watch.scheduled_job("interval", seconds=30)
+async def watch_post():
+    # logger.info('开始检查微博更新...')
     for k, v in wb.source_data.items():
         data_t = await get_data(k)
+        # 跳过订阅群组为空的微博记录
+        if not v.get("group_ids"):
+            # logger.warning(f'{v.get("screen_name")} skip here!!!!!')
+            continue
         bid = int(data_t.get("bid"))
-        logger.info(f'检查 {v.get("screen_name")} 微博更新...')
+        # logger.info(f'检查 {v.get("screen_name")} 微博更新...')
         if bid <= v.get("last"):
             continue
         else:
+            logger.info(f'检查到 {v.get("screen_name")} 微博更新...')
             # 修改最新bid
             wb.modify_bid(k, bid)
             msg = format_mblog(data_t)
@@ -37,7 +43,7 @@ async def run_every_10_min():
                     'message': msg,
                     'group_id': group
                 })
-                logger.success(f'向{group}发送{msg[:10]}')
+                logger.success(f'向{group}发送：{v.get("screen_name")}')
 
 
 def save_data(uid, dict_remote):
@@ -49,6 +55,8 @@ def format_mblog(mblog):
     """
     将消息格式化
     """
+    if not mblog.get("bid"):
+        return '该账号暂无最新微博~'
     msg = MessageSegment.text(f'{mblog["screen_name"]}\n============\n{mblog["text"]}\n')
     for i in mblog.get("images"):
         msg += MessageSegment.image(i) + '\n'
@@ -73,26 +81,39 @@ async def _(event: GroupMessageEvent, par: Message = CommandArg()):
         await add_wb_subscribe.finish('请输入要订阅微博用户的uid~')
     else:
         uid = par.extract_plain_text()
-        name = await wb.add_subscribe(uid, event.group_id)
+        data = await get_data(par.extract_plain_text())
+        name = await wb.add_subscribe(uid, event.group_id, int(data.get("bid")))
         if not name:
             await add_wb_subscribe.finish('已经订阅过了~')
         else:
-            msg += name
-        await add_wb_subscribe.send(msg)
-        data = await get_data(par.extract_plain_text())
-        msg = format_mblog(data)
-        await add_wb_subscribe.finish(msg)
+            await add_wb_subscribe.send(msg + name)
+            await add_wb_subscribe.finish(format_mblog(data))
 
 
 @rm_wb_subscribe.handle()
-async def _(event: GroupMessageEvent):
-    pass
+async def _(event: GroupMessageEvent, par: Message = CommandArg()):
+    no = par.extract_plain_text()
+    if not no:
+        # 参数为空
+        await add_wb_subscribe.finish('请输入要退订微博用户对应的编号~')
+    elif not no.isdigit():
+        # 参数不是数字
+        await add_wb_subscribe.finish('ERROR: 未知编号')
+    else:
+        container_list = wb.get_container_list(event.group_id)
+        group = event.group_id
+        if no not in container_list:
+            await add_wb_subscribe.finish('ERROR: 未知编号')
+        await wb.rm_subscribe(container_list.get(no), group)
+        await add_wb_subscribe.finish('Operation complete.')
 
 
 @ls_wb_subscribe.handle()
 async def _(event: GroupMessageEvent):
-    groups = wb.group_sub_list(event.group_id)
+    sub_list = wb.group_sub_list(event.group_id)
     msg = '订阅列表：'
-    for i in groups:
-        msg += f'\n- {i}'
+    num = 0
+    for k, v in sub_list.items():
+        msg += f'\n{num}. {v}'
+        num += 1
     await ls_wb_subscribe.finish(msg)
