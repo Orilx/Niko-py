4ec20da0d4180cafc2008580b23be0657cb2af6c
Dr.Ink <x2590633525@outlook.com>
2022-02-14 00:10:14 +0800

    feat 新增课表查询功能

--- plugins/sub_config/services.py
+++ plugins/sub_config/services.py
@@ -1,6 +1,6 @@
 from utils.config_util import SubConfig
 
-c_schedule_sub = SubConfig('c_schedule_sub')
+c_schedule_sub = SubConfig('cs_sub')
 honor_sub = SubConfig('honor_sub')
 
 key_words = {'每日课表': c_schedule_sub,
