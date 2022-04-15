0c2ce7e5e8faa3ad5288959fbaf876b184b582db
Dr.Ink <x2590633525@outlook.com>
2022-04-15 16:47:29 +0800

    refactor 适配 mcsm9.0 api

--- plugins/mc_server/__init__.py
+++ plugins/mc_server/__init__.py
@@ -1,21 +1,55 @@
 from nonebot import on_command
+from nonebot.adapters.onebot.v11 import GroupMessageEvent
+
 from utils.utils import get_json
+from utils.config_util import sub_config
 
 status = on_command('status')
 
-server_url = 'https://mc.orilx.top/api/status/new_world'
+d = {
+    "server_url": "",
+    "uuid": "",
+    "remote_uuid": "",
+    "apikey": ""
+}
+conf = sub_config.register("mc_status", d)
+print(conf)
+
 
 @status.handle()
-async def status_():
-    
-    js = await get_json(server_url)
+async def _(event: GroupMessageEvent):
+    if event.group_id not in conf["group_id"]:
+        await status.finish()
+
+    query = {
+        "uuid": conf["uuid"],
+        "remote_uuid": conf["remote_uuid"],
+        "apikey": conf["apikey"],
+    }
+    headers = {
+        "accept": "application/json"
+    }
+    js = await get_json(conf["server_url"], query, headers)
+    if not js:
+        await status.finish('エラー発生')
+
+    data = js['data']
     msg = ''
-    if js['status']:
-        if "lastDate" in js:
-            msg += '服务器正在启动...'
+
+    if data["status"] == 0:
+        msg += f'服务器当前状态：关闭\n上次启动时间：{data["config"]["lastDatetime"]}'
+
+    elif data["status"] == 3:
+        if data["info"]["version"]:
+            time = int(data["processInfo"]["elapsed"]) / 1000
+            d = int(time / (24 * 3600))
+            h = int((time / 3600) % 24)
+            m = int((time / 60) % 60)
+            s = int(time % 60)
+            msg += f'服务器名称：{data["config"]["nickname"]}\n当前状态：开启\n' \
+                   f'启动时间：{data["config"]["lastDatetime"]}\n服务端版本：{data["info"]["version"]}\n' \
+                   f'在线人数：{data["info"]["currentPlayers"]}/{data["info"]["maxPlayers"]}\n已运行：{d}天{h}时{m}分{s}秒'
         else:
-            msg += f'服务器当前状态：开启\n服务器名称：{js["serverName"]}\n服务端版本：{js["version"]}\n在线人数：{js["current_players"]}/{js["max_players"]}'
-        
-    else:
-        msg += f'服务器当前状态：关闭\n上次启动时间：{js["lastDate"]}'
+            msg += '服务器正在启动...'
+
     await status.finish(msg)
--- utils/utils.py
+++ utils/utils.py
@@ -8,6 +8,9 @@ from nonebot.adapters.onebot.v11 import exception
 
 
 async def send_group_msg(group_id: int, msg):
+    """
+    主动向群组发送消息
+    """
     try:
         await nonebot.get_bot().call_api('send_group_msg', **{
             'message': msg,
@@ -20,14 +23,17 @@ async def send_group_msg(group_id: int, msg):
         return True
 
 
-async def get_json(url: str, params=None):
+async def get_json(url: str, params=None, headers=None):
     if params is None:
         params = {}
     async with AsyncClient() as client:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/54.0.2840.99 Safari/537.36"}
-        res = await client.get(url, params=params, headers=headers, timeout=30)
+        if headers:
+            client.headers = headers
+        else:
+            client.headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/54.0.2840.99 Safari/537.36"}
+        res = await client.get(url, params=params, timeout=30)
     return res.json()
 
 
@@ -41,3 +47,11 @@ def get_diff_days_2_now(date_str):
     date2 = datetime.datetime(now_time[0], now_time[1], now_time[2])
     diff_days = (date2 - date1).days
     return diff_days
+
+
+def is_overlap(range_a: tuple, range_b: tuple):
+    """
+    检查两区间是否重叠
+    传入参数为两个包含开始和结束周的元组
+    """
+    return max(range_a[0], range_b[0]) <= min(range_a[1], range_b[1])
