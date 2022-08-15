8b5b66cc2c7d6b545186c68ae791de71a330e1f8
Orilx <2590633525@outlook.com>
2022-08-16 00:36:48 +0800

    :poop: 新增 mqtt 客户端实现

---
+++ plugins/gate_controller/__init__.py
@@ -0,0 +1,34 @@
+import json
+from asyncio import sleep
+
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import GroupMessageEvent
+
+from utils.cq_utils import send_group_msg
+from utils.mqtt import create_connection
+
+get_temp = on_command('get_temp', priority=5, block=True)
+
+
+@get_temp.handle()
+async def _(event: GroupMessageEvent):
+    group_id = event.group_id
+    qid = event.user_id
+
+    async def on_msg_callback(client, topic, payload, qos, properties):
+        data = json.loads(payload.decode('utf-8'))
+        m = f'temp: {data["temp"]}℃\nhumi: {data["humi"]}%\nbatt: {data["batt"]}%\ntime: {data["time"]}'
+        if data["batt"] == -1:
+            m = data["remark"]
+        await send_group_msg(group_id, m)
+        await client.disconnect()
+
+    def on_conn_callback(client, flags, rc, properties):
+        client.publish('test/mi_sensor', {'flag': 1})
+    temp = await create_connection(f'{group_id}_{qid}', on_message=on_msg_callback, on_connect=on_conn_callback)
+    temp.client.subscribe('test/temp')
+    await get_temp.send('正在获取传感器信息...')
+    await sleep(15)
+    if temp.client.is_connected:
+        await temp.disconnect()
+        await get_temp.send('获取数据超时，请稍后再试！')
---
+++ utils/mqtt.py
@@ -0,0 +1,62 @@
+from gmqtt import Client
+from nonebot.log import logger
+from utils.config_util import ConfigManager
+
+data = {
+    "host": "",
+    "port": -1,
+    "client_id": "",
+    "user": "",
+    "password": "",
+    "pub_topic": "",
+    "sub_topic": ""
+}
+
+conf_data = ConfigManager.register("mqtt", data)
+
+
+class MQTTClient:
+    def __init__(self, conf, client_id, **kwargs):
+        self.port = conf["port"]
+        self.host = conf["host"]
+        self.client_id = client_id
+        self.client = Client(client_id)
+        self.client.set_auth_credentials(conf["user"], conf["password"])
+
+        # asyncio.ensure_future(self.connect())
+        self.client.on_connect = kwargs.pop('on_connect', self.on_connect)
+        self.client.on_message = kwargs.pop('on_message', self.on_message)
+        self.client.on_disconnect = kwargs.pop('on_disconnect', self.on_disconnect)
+        self.client.on_subscribe = kwargs.pop('on_subscribe', self.on_subscribe)
+
+    async def connect(self):
+        await self.client.connect(self.host, self.port)
+
+    async def disconnect(self):
+        await self.client.disconnect()
+
+    def on_connect(self, client, flags, rc, properties):
+        logger.info(f"Mqtt client [{self.client_id}] connected.")
+
+    def on_message(self, client, topic, payload, qos, properties):
+        msg = f"Mqtt client [{self.client_id}] topic:[{topic}] receive msg {payload.decode('utf-8')}"
+        logger.info(msg)
+
+    def on_disconnect(self, client, packet, exc=None):
+        logger.info(f'Mqtt client [{self.client_id}] disconnected.')
+
+    def on_subscribe(self, client, mid, qos, properties):
+        # logger.info(f'Mqtt topic [{self.topic}] subscribed.')
+        logger.info(f'Mqtt topic subscribed.')
+
+
+async def create_connection(client_id, **kwargs):
+    client = MQTTClient(conf_data, client_id, **kwargs)
+    # 确保建立连接之后再返回client实例
+    await client.connect()
+    return client
+
+
+async def conn_and_subscribe(client_id, **kwargs):
+    client = MQTTClient(conf_data, client_id, **kwargs)
+    return client
